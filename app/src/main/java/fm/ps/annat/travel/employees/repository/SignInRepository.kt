9b58fc4eed759c3441daaee727cde94aa8d27ccb
Moath Raed Abu Ouda <85387360+Fmoath@users.noreply.github.com>
2022-02-20 01:23:37 +0200

    Create Repository -> Sign In Repository -> V

---
+++ app/src/main/java/fm/ps/annat/travel/employees/repository/SignInRepository.kt
@@ -0,0 +1,14 @@
+package fm.ps.annat.travel.employees.repository
+
+/**
+* Created With Love For : Annat Travel
+* The owner has been granted all rights in the application and has the freedom to dispose of everything
+* @by : Eng-Moath Raed Abu Ouda
+* @CompanyManager : FM-Team 
+* @In : Palestine - Gaza - Biet Hanoun
+* @OnDate : 2/20/2022
+* @InTheHour : 01 : 22 : 02
+* @Website : fm-ps.online
+* @Whatsapp : 00972597718418
+*/class SignInRepository {
+}
