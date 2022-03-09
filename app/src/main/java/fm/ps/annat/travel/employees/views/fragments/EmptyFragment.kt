c184f709d34fe580d3115240e49761c16d71afeb
Moath Raed Abu Ouda <85387360+Fmoath@users.noreply.github.com>
2022-03-09 11:26:39 +0200

    Create Fragment -> Empty Fragment -> D&Bo

---
+++ app/src/main/java/fm/ps/annat/travel/employees/views/fragments/EmptyFragment.kt
@@ -0,0 +1,63 @@
+package fm.ps.annat.travel.employees.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import fm.ps.annat.travel.employees.databinding.FragmentEmptyBinding
+import fm.ps.annat.travel.employees.R
+
+/**
+ * Created With Love For : Annat Travel
+ * The owner has been granted all rights in the application and has the freedom to dispose of everything
+ * @by : Eng-Moath Raed Abu Ouda
+ * @CompanyManager : FM-Team
+ * @In : Palestine - Gaza - Biet Hanoun
+ * @OnDate : 3/9/2022
+ * @InTheHour : 11 : 25 : 12
+ * @Website : fm-ps.online
+ * @Whatsapp : 00972597718418
+ */
+class EmptyFragment : BaseFragment() {
+
+    //==============================================================================================
+    // Binding ...
+    private lateinit var binding: FragmentEmptyBinding
+
+    //==============================================================================================
+    // Variable ...
+
+    //==============================================================================================
+    // On Create Fragment  ...
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_empty, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeFragment()
+    }
+
+    //==============================================================================================
+    // Initialize Fragment ...
+    private fun initializeFragment() {
+        initializeView()
+        listener()
+    }
+
+    private fun initializeView() {
+
+    }
+
+    //==============================================================================================
+    // Listener ...
+    private fun listener() {
+
+    }
+}
